2a0e373d112a9a2fde9ed83e3a9827a0e3201bb8
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 16:45:29 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,3 +9,11 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kid}! You are now #{age} years old!"
   end
 end
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kid,age|
+    if(age<=12)
+      puts "Happy Birthday #{kid}! You are now #{age} years old!"
+    end
+  end
+end
